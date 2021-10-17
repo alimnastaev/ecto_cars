c9439c661bd2d27b64fbfa55d607c93a94edf955
alimnastaev <a.nastaev@gmail.com>
2021-10-17 17:02:08 -0400

    DESCRIPTION:
    1- dfsadsafds
    2- dsaggdfgfdrrrtt
    3- 90294032840382432
    4- jfkjljdsljfdsf

---
+++ lib/ecto_cars.ex
@@ -0,0 +1,19 @@
+defmodule EctoCars do
+  @moduledoc """
+  Documentation for EctoCars.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoCars.hello()
+      :world
+
+  """
+  def hello do
+    "world"
+    "test"
+  end
+end
